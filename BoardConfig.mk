7a819a7546ca8ff17724525189a4e574f75ee274
Marc-Rm14 <marcosrm142000@gmail.com>
2026-09-10 23:15:37 -0600

    BoardConfig: fix cmdline location, add BOARD_KERNEL_IMAGE_NAME

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,6 @@ TARGET_NO_BOOTLOADER := true
 # ======================
 # Kernel (header v4 para Android 16 GKI)
 # ======================
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_OFFSET := 0x40000000
@@ -163,3 +162,10 @@ BOARD_BOOT_HEADER_VERSION := 4
 
 # AVB (minimal, como transsion mt6789-common)
 BOARD_AVB_ENABLE := true
+
+# Vendor cmdline (extraída del vendor_boot.img original)
+BOARD_VENDOR_CMDLINE := bootopt=64S3,32N2,64N2 bootconfig
+BOARD_MKBOOTIMG_ARGS += --vendor_cmdline $(BOARD_VENDOR_CMDLINE)
+
+# Kernel image name (GKI ARM64)
+BOARD_KERNEL_IMAGE_NAME := Image
