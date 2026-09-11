4e0c85a326b8297caf4057bd11fc08a75d9b3d59
Marc-Rm14 <marcosrm142000@gmail.com>
2026-09-10 21:08:32 -0600

    Restructure: FOX_/OF_ vars moved to vendorsetup.sh, fix armv8-2a, exact partition sizes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,43 @@
-# OrangeFox vendorsetup for spinel
+#!/bin/bash
+# OrangeFox vendorsetup for Redmi Note 15 (spinel)
+
+# Product spec
 add_lunch_combo twrp_spinel-eng
+
+# ======================
+# OrangeFox build variables
+# ======================
+
+# A/B device (punto E de la doc)
+export FOX_AB_DEVICE=1
+
+# Virtual A/B device (punto F de la doc)
+export FOX_VIRTUAL_AB_DEVICE=1
+
+# Recovery va en vendor_boot
+export FOX_VENDOR_BOOT_RECOVERY=1
+
+# Kernel prebuilt (punto H de la doc)
+export OF_FORCE_PREBUILT_KERNEL=1
+
+# Keymaster version (punto J de la doc)
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+
+# Device name
+export FOX_BUILD_DEVICE=spinel
+
+# Features
+export OF_USE_GREEN_LED=0
+export OF_FLASHLIGHT_ENABLE=0
+export OF_SCREEN_H=2400
+export OF_STATUS_H=90
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_HIDE_NOTCH=1
+export OF_ALLOW_DISABLE_NAVBAR=1
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export OF_ENABLE_LPTOOLS=1
+export OF_UNMOUNT_SYSTEM=1
