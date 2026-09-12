cc651b0341a4f98116b8226bf64a40170bb2cae9
Marc-Rm14 <Marcosrm142000@gmail.com>
2026-09-12 17:54:02 -0600

    Update twrp_spinel.mk to remove unused configurations
    Removed dynamic partitions and GMS client ID settings.

--- twrp_spinel.mk
+++ twrp_spinel.mk
@@ -1,9 +1,9 @@
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-ENABLE_VIRTUAL_AB := true
 PRODUCT_TARGET_VNDK_VERSION := 34
 PRODUCT_SHIPPING_API_LEVEL := 34
 
@@ -14,5 +14,3 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 15
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_RELEASE_NAME := spinel
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
