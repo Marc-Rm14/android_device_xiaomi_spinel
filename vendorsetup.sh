06bcd47bd106b94d021d420f2de382c5c64e98c3
Marc-Rm14 <Marcosrm142000@gmail.com>
2026-09-12 21:49:39 -0600

    Remove FOX_REFERENCE_VENDOR_BOOT_IMAGE variable
    Remove reference to stock vendor boot image.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -56,5 +56,4 @@ export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 export OF_ENABLE_LPTOOLS=1
 export OF_UNMOUNT_SYSTEM=1
-export FOX_REFERENCE_VENDOR_BOOT_IMAGE=$(gettop)/device/xiaomi/spinel/prebuilt/stock_vendor_boot.img
 export FOX_ADD_API_V36_PREBUILTS=1
