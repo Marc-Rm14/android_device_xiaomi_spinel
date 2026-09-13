fe1ddb2449b66efd5fcd0ebb1420bb53a6cd888a
Marc-Rm14 <Marcosrm142000@gmail.com>
2026-09-12 18:40:00 -0600

    Add copyright and license details to vendorsetup.sh
    Added copyright and license information to vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,23 @@
+#
+# Copyright (C) 2026 The OrangeFox Recovery Project
+#
+# This file is part of the OrangeFox Recovery Project
+#
+# OrangeFox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OrangeFox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# See <http://www.gnu.org/licenses/>.
+#
+
 #!/bin/bash
+
 # OrangeFox vendorsetup for Redmi Note 15 (spinel)
 
 # Product spec
