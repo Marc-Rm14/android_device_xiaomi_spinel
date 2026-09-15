f4d5c096cf5dbadf8e3412ace869884925af44c5
Marc-Rm14 <Marcosrm142000@gmail.com>
2026-09-14 19:59:59 -0600

    Update CPU variant settings in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,19 +7,20 @@ DEVICE_PATH := device/xiaomi/spinel
 # ======================
 # Architecture
 # ======================
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a55
-TARGET_CPU_VARIANT_RUNTIME := cortex-a55
-TARGET_CPU_SMP := true
+TARGET_ARCH                     := arm64
+TARGET_ARCH_VARIANT             := armv8-a
+TARGET_CPU_ABI                  := arm64-v8a
+TARGET_CPU_VARIANT              := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME      := cortex-a76
+TARGET_CPU_SMP                  := true
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_VARIANT := cortex-a55
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
-TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_2ND_ARCH                 := arm
+TARGET_2ND_ARCH_VARIANT         := armv8-2a
+TARGET_2ND_CPU_ABI              := armeabi-v7a
+TARGET_2ND_CPU_ABI_2            := armeabi
+TARGET_2ND_CPU_VARIANT          := cortex-a55
+TARGET_2ND_CPU_VARIANT_RUNTIME  := cortex-a55
+TARGET_SUPPORTS_64_BIT_APPS     := true
 
 TARGET_BOARD_PLATFORM := mt6789
 TARGET_BOOTLOADER_BOARD_NAME := spinel
